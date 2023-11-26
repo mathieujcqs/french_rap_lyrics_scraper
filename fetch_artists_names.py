a52ba2f6395c4d2347d39e7730a010b00c9845dd
mathieujcqs <m.jacques139@gmail.com>
2023-11-26 20:54:34 +0100

    Feat: add lyrics scraper

---
+++ fetch_artists_names.py
@@ -0,0 +1,154 @@
+import time
+import random as rd
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+from file_utils import get_spotify_cred, get_config, write_in_config
+
+def search_french_rap_playlists(sp, offsets):
+    """
+    Fetches French hip hop playlists from Spotify.
+
+    Parameters:
+    sp (Spotify object): Spotify client object.
+    offsets (list of int): List of offsets for pagination.
+
+    Returns:
+    list: List of playlist IDs.
+    """
+    all_playlist_ids = []
+
+    for offset in offsets:
+        print(f'Fetching playlists starting from offset: {offset}')
+        response = sp.search(q="genre: french hip hop", limit=50, type="playlist", offset=offset)
+        all_playlist_ids.extend([item["id"] for item in response["playlists"]["items"]])
+
+        while response["playlists"]["next"]:
+            response = sp.next(response["playlists"])
+            all_playlist_ids.extend([item["id"] for item in response["playlists"]["items"]])
+
+    return all_playlist_ids
+
+
+def get_playlist_tracks(sp, playlist_id):
+    """
+    Retrieves tracks from a specific Spotify playlist.
+
+    Parameters:
+    sp (Spotify object): Spotify client object.
+    playlist_id (str): The Spotify ID of the playlist.
+
+    Returns:
+    list: List of tracks in the playlist.
+    """
+    tracks = []
+
+    result = sp.playlist_items(playlist_id)
+    tracks.extend(result["items"])
+
+    while result["next"]:
+        result = sp.next(result)
+        tracks.extend(result["items"])
+
+    return tracks
+
+
+def get_playlists_tracks(sp, playlist_ids):
+    """
+    Retrieves tracks from multiple Spotify playlists.
+
+    Parameters:
+    sp (Spotify object): Spotify client object.
+    playlist_ids (list of str): List of Spotify playlist IDs.
+
+    Returns:
+    list: List of tracks from all specified playlists.
+    """
+    all_tracks = []
+
+    for count, playlist_id in enumerate(playlist_ids, start=1):
+        print(f'Fetching tracks from playlist {count}')
+        tracks = get_playlist_tracks(sp, playlist_id)
+        all_tracks.extend(tracks)
+
+    print(f"Fetched {len(all_tracks)} tracks")
+
+    return all_tracks
+
+
+def get_artists_ids_from_tracks(tracks):
+    """
+    Extracts unique artist IDs from a list of tracks.
+
+    Parameters:
+    tracks (list): A list of track dictionaries.
+
+    Returns:
+    set: A set of unique artist IDs.
+    """
+    artists_ids = {track["track"]["artists"][0]["id"] for track in tracks if track["track"]}
+
+    print(f'Fetched {len(artists_ids)} unique artists')
+
+    return artists_ids
+
+
+def get_tracks_artists_spotify(sp, artist_ids):
+    """
+    Retrieves artist names from Spotify based on their IDs if they belong to the 'french hip hop' genre.
+
+    Parameters:
+    sp (Spotify object): Spotify client object.
+    artist_ids (set of str): Set of artist IDs.
+
+    Returns:
+    set: A set of artist names.
+    """
+    artist_names = set()
+    
+    for idx, artist_id in enumerate(artist_ids):
+        # Adding extra sleep time to avoid getting timed out
+        if idx % 1000 == 0:
+            time.sleep(10)
+        
+        time.sleep(rd.uniform(0.2, 0.7))
+        print(f"Fetching artist {artist_id}")
+        result = sp.artist(artist_id)
+
+        if "genres" in result and "french hip hop" in result["genres"]:
+            print(result["name"])
+            artist_names.add(result["name"])
+
+    return artist_names
+
+
+def main():
+
+    spotify_creds = get_spotify_cred("spotify_cred.json")
+
+    client_cred_manager = SpotifyClientCredentials(
+        client_id=spotify_creds["cid"], 
+        client_secret=spotify_creds["cis"]
+    )
+
+    sp_auth = SpotifyOAuth(client_id=spotify_creds["cid"],
+            client_secret=spotify_creds["cis"],
+            redirect_uri="http://127.0.0.1:8000/",
+            scope="user-library-read"
+            )
+    
+    sp = spotipy.Spotify(client_credentials_manager=client_cred_manager,auth= sp_auth.get_cached_token())
+    playlist_ids = search_french_rap_playlists(sp, [0, 50, 100, 150, 200])
+    tracks = get_playlists_tracks(sp, playlist_ids)
+    artist_ids = get_artists_ids_from_tracks(tracks)
+    artist_names = get_tracks_artists_spotify(sp, artist_ids)
+
+    
+    config_data = get_config("main.yml")
+    config_data['artists_names'] = sorted(list(artist_names))
+    write_in_config("main.yml", config_data)
+
+    print('Artist names saved to main.yml under artists_names key')
+
+if __name__ == "__main__":
+    main()
+
---
+++ fetch_lyrics_per_artist.py
@@ -0,0 +1,186 @@
+import os
+import re
+import json
+import time
+import requests
+import random as rd
+from bs4 import BeautifulSoup
+from file_utils import get_genius_cred, get_config
+
+def get_artists_ids(api_base_url, headers, artists_names):
+    """Fetch artist IDs based on artist names.
+
+    Args:
+    api_base_url (str): The base URL of the API.
+    headers (dict): The headers to include in the API request.
+    artists_names (list of str): A list of artist names.
+
+    Returns:
+    list of int: A list of artist IDs.
+    """
+    artists_ids = {}
+
+    for artist_name in artists_names:
+        params = {"q": artist_name}
+        print(f"Looking for {artist_name} id")
+        response = requests.get(f"{api_base_url}/search/", params=params, headers=headers)
+        response_json = response.json()
+
+        artist_id = None
+        for hit in response_json['response']['hits']:
+            if (hit["result"]["primary_artist"]["name"].lower() == artist_name.lower()) and (hit["type"] == "song"):
+                artist_id = hit["result"]["primary_artist"]["id"]
+                break
+
+        if artist_id is not None:
+            print("Collecting songs for", artist_name, ": Artist ID", artist_id)
+            artists_ids[artist_id] = artist_name
+        else:
+            print("Artist", artist_name, "not found.")
+
+    print("IDs found")
+    return artists_ids
+
+def get_artist_songs_url(api_base_url, headers, artist_id):
+    """Fetch URLs of all songs by a specific artist from the Genius API.
+
+    Args:
+    api_base_url (str): The base URL of the API.
+    headers (dict): The headers to include in the API request.
+    artist_id (int): The unique identifier for the artist.
+
+    Returns:
+    list of tuples: A list containing tuples with the song title and song URL.
+    """
+    artist_songs_url = {}
+
+    page_count = 1
+    while True:
+        params = {"per_page": 50, "page": page_count}
+        response = requests.get(
+            f"{api_base_url}/artists/{artist_id}/songs",
+            params=params,
+            headers=headers
+        )
+        response_json = response.json()
+        
+        for song in response_json['response']['songs']:
+            if song["primary_artist"]["id"] == artist_id:
+                artist_songs_url[song['title']] =  song["path"]
+                
+        if response_json['response'].get('next_page') is None:
+            break
+        else:
+            page_count += 1
+
+        print(len(artist_songs_url), "songs found")
+
+    return artist_songs_url
+
+def extract_verse_refrain(lyrics):
+    """Extract verses and refrains from lyrics.
+
+    Args:
+    lyrics (str): A string containing the song lyrics.
+
+    Returns:
+    tuple: A tuple containing two lists, one with the verses and one with the refrains.
+    """
+    verse_pattern = r'\[Couplet \d+\]\n(.*?)(?=\[Refrain\]|\Z)'
+    refrain_pattern = r'\[Refrain\]\n(.*?)(?=\[Couplet \d+\]|\Z)'
+
+    # Extract Couplet and Refrain sections
+    verses = re.findall(verse_pattern, lyrics, re.DOTALL)
+    refrains = re.findall(refrain_pattern, lyrics, re.DOTALL)
+
+    return verses, refrains
+
+def get_song_lyrics(base_url, song_urls, min_sleep_time, max_sleep_time):
+    """
+    Fetch and store song lyrics categorized by verses and refrains.
+
+    Args:
+    base_url (str): The base URL.
+    song_urls (list of tuples): A list of tuples containing song names and URLs.
+    min_sleep_time (float): Minimum sleep time between two scrapes of the Genius website.
+    max_sleep_time (float): Maximum sleep time between two scrapes of the Genius website.
+
+    Returns:
+    dict: A dictionary containing song lyrics categorized by verses and refrains.
+    """
+    artist_lyrics = {}
+
+    for song_name, url in song_urls:
+        try:
+            time.sleep(rd.uniform(min_sleep_time, max_sleep_time))
+            response = requests.get(f"{base_url}{url}")
+
+            if response.status_code == 200:
+                artist_lyrics[song_name] = {}
+
+                html = BeautifulSoup(response.text, "html.parser")
+                div = html.find("div", class_=re.compile("^lyrics$|Lyrics__Root"))
+
+                if div:
+                    lyrics = div.get_text(separator="\n")
+                    song_verses, song_refrains = extract_verse_refrain(lyrics)
+                    artist_lyrics[song_name]['lyrics'] = lyrics
+                    artist_lyrics[song_name]['verses'] = song_verses
+                    artist_lyrics[song_name]['refrains'] = song_refrains
+                else:
+                    print(f"Lyrics not found for {song_name}")
+
+            elif response.status_code == 429:  # Rate limited
+                reset_time = int(response.headers.get('X-RateLimit-Reset', 0)) - int(time.time())
+                reset_time = max(0, reset_time)
+                print(f"Rate limit exceeded. Sleeping for {reset_time} seconds.")
+                time.sleep(reset_time)
+                continue  # retry the current song
+
+            else:
+                print(f"Error fetching {song_name}. HTTP Status Code: {response.status_code}")
+
+        except requests.RequestException as e:
+            print(f"An error occurred: {e}")
+            continue
+
+    return artist_lyrics
+
+def main(CONFIG, headers):
+    artists_names = CONFIG['artists_names']
+    base_url = CONFIG['genius_base_url']
+    api_base_url = CONFIG['genius_api_base_url']
+    artists_lyrics_dir = CONFIG["artists_lyrics_dir"]
+    #proxies = CONFIG["proxies"]
+    min_sleep_time = CONFIG['min_sleep_time']
+    max_sleep_time = CONFIG['max_sleep_time']
+
+    os.makedirs(artists_lyrics_dir, exist_ok=True)
+
+    artists_ids = get_artists_ids(api_base_url, headers, artists_names)
+
+    for artist_id, artist_name in artists_ids.items():
+        song_urls = get_artist_songs_url(api_base_url, headers, artist_id)
+        print(f'Fetching {len(song_urls)} songs for artist: {artist_name}')
+        artist_lyrics = get_song_lyrics(base_url, song_urls, min_sleep_time, max_sleep_time)
+        
+        safe_artist_name = "".join(
+            c if c.isalnum() or c in " ._-()" else "_"
+            for c in artist_name
+        )
+
+        # Constructing the file path
+        file_path = os.path.join(artists_lyrics_dir, f"{safe_artist_name}.json")
+
+        # Writing the lyrics data to the file
+        with open(file_path, "w", encoding="utf-8") as file:
+            json.dump(artist_lyrics, file, ensure_ascii=False, indent=4)
+
+        print(f"Lyrics for {artist_name} saved to {file_path}")
+
+if __name__ == '__main__':
+    genius_cred = get_genius_cred("genius_cred.json")
+    CONFIG = get_config("main.yml")
+    headers= { 'Authorization' : f"Bearer {genius_cred['cat']}"}
+    main(CONFIG, headers)
+     
